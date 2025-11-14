9fe764013fda24bf526fa96a3e8c45cba0a70d5a
SamuelFernandes2009 <s.samuel21@escola.pr.gov.br>
2025-11-14 08:59:18 -0300

    Refactor button click event handling

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
+        // Remove a classe 'ativo' do passo atual
+        atual.classList.remove('ativo');
+
+        // Adiciona a classe 'ativo' no próximo passo para exibi-lo
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
